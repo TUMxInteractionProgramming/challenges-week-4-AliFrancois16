2d3e2a92651189118f1ea98664ed114be53d4830
AliFrancois16 <37365993+AliFrancois16@users.noreply.github.com>
2018-09-26 08:46:34 +0200

    Challenge 7

---
+++ js/channels.js
@@ -0,0 +1,41 @@
+var yummy, sevenContinents, killerApp, firstPersonOnMars, octoberfest;
+yummy = {
+    name: "#Yummy",
+    createdOn: new Date("April 1, 2016"),
+    createdBy: "minus.plus.yummy",
+    starred: false,
+    expiresIn: 100 * 60 * 1000,
+    messageCount: 999
+};
+sevenContinents = {
+    name: "#SevenContinents",
+    createdOn: new Date("April 2, 2016"),
+    createdBy: "cheeses.yard.applies",
+    starred: true,
+    expiresIn: 100 * 60 * 1000,
+    messageCount: 999
+};
+killerApp = {
+    name: "#KillerApp",
+    createdOn: new Date("April 1, 2016"),
+    createdBy: "minus.plus.yummy",
+    starred: false,
+    expiresIn: 100 * 60 * 1000,
+    messageCount: 999
+};
+firstPersonOnMars = {
+    name: "#FirstPersonOnMars",
+    createdOn: new Date("Septembre 28, 2016"),
+    createdBy: "snipped.atom.grid",
+    starred: true,
+    expiresIn: 100 * 60 * 1000,
+    messageCount: 999
+};
+octoberfest = {
+    name: "#Octoberfest",
+    createdOn: new Date("April 1, 2016"),
+    createdBy: "minus.plus.yummy",
+    starred: false,
+    expiresIn: 100 * 60 * 1000,
+    messageCount: 999
+};
--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+var currentLocation, currentChannel = sevenContinents;
+currentLocation = {
+    longitude: 2.297832,
+    latitude: 48.855736,
+    what3words: "fault.ages.history"
+};
+
 /* #6 start the #external #action and say hello */
 console.log("App is alive");
 
@@ -5,28 +12,33 @@ console.log("App is alive");
  * #6 #Switcher function for the #channels name in the right app bar
  * @param channelName Text which is set
  */
+
 function switchChannel(channelName) {
+    currentChannel = channelName;
     //Log the channel switch
-    console.log("Tuning in to channel", channelName);
+    console.log("Tuning in to channel", currentChannel);
 
     //Write the new channel to the right app bar
-    document.getElementById('channel-name').innerHTML = channelName;
+    document.getElementById('channel-name').innerHTML = channelName.name;
 
     //#6 change the #channel #location
-    document.getElementById('channel-location').innerHTML = 'by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>upgrading.never.helps</strong></a>';
+    document.getElementById('channel-location').innerHTML = 'by <a href="http://w3w.co/upgrading.never.helps" target="_blank"><strong>' + channelName.createdBy + '</strong></a>';
 
     /* #6 #liking channels on #click */
-    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star-o.png');
+    $('#channel-star').attr('class', (channelName.starred ? 'fas' : 'far') + ' fa-star');
 
     /* #6 #highlight the selected #channel.
        This is inefficient (jQuery has to search all channel list items), but we'll change it later on */
     $('#channels li').removeClass('selected');
-    $('#channels li:contains(' + channelName + ')').addClass('selected');
+    $('#channels li:contains(' + channelName.name + ')').addClass('selected');
 }
 
 /* #6 #liking a channel on #click */
 function star() {
-    $('#channel-star').attr('src', 'http://ip.lfe.mw.tum.de/sections/star.png');
+    $('#channel-star').toggleClass('fas');
+    $('#channel-star').toggleClass('far');
+    currentChannel.starred = !currentChannel.starred;
+    $('li.selected span.channel-meta i.fa-star').attr('class', (currentChannel.starred ? 'fas' : 'far') + ' fa-star');
 }
 
 /**
